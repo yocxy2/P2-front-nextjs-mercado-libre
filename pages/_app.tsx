e03d95fb1616678e8b259e8dc6aa7da846b5d50c
Victor Manuel Osorio Garcia <81490294+victor013001@users.noreply.github.com>
2023-04-17 16:28:37 -0500

    toastify añadido

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <ToastContainer />
+  </>
 );
 
 export default App;
