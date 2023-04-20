493c337c5e1e37443b2ec80492e0e6fde356d23d
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-04-20 11:47:54 -0500

    Componenete de card beneficios terminado

--- components/cardBeneficions.tsx
+++ components/cardBeneficions.tsx
@@ -12,16 +12,22 @@ interface CardBeneficionsProps{
 const CardBeneficions = ({backImage, backName, forwardName, forwardImage, text1, text2, text3}: CardBeneficionsProps) => {
   return (
     <>
-      <div className='relative h-[330px] w-[335px] debug'>
-          <Image className= "rounded-md" src={backImage} alt={backName} width={335} height={250}/>
-          <div className='-mt-24 ml-4 flex gap-4 h-[60px] '>
-              <Image src={forwardImage} alt={forwardName} width={60} height={60}/>
-              <div className='flex flex-col gap-[5px] pt-[5px] pb-[15px]'>
-                  <span className='font-sans text-white text-xs'>{text1}</span>
-                  <span className='font-sans text-white text-xl'>{text2}</span>
-                  <span className='font-sans text-white'>{text3}</span>
-              </div>
+      <div className='relative h-[206px] w-[315px]'>
+        <Image
+          className='rounded-md'
+          src={backImage}
+          alt={backName}
+          width={315}
+          height={250}
+        />
+        <div className='debug flex -mt-20 pl-4 gap-4 h-[60px]'>
+          <Image src={forwardImage} alt={forwardName} width={60} height={60} />
+          <div className='flex flex-col gap-[5px] pt-[5px] pb-[15px]'>
+            <span className='font-sans text-white text-xs'>{text1}</span>
+            <span className='font-sans text-white text-xs'>{text2}</span>
+            <span className='font-sans text-white text-xs'>{text3}</span>
           </div>
+        </div>
       </div>
     </>
   );
